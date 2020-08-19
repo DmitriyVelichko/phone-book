1c944b7a511a7e997447b5ccb4326acaacd9b511
dm.velichko <dm.velichko@gmail.com>
2020-08-19 18:50:25 +0300

    сортировка работает по алфавиту

--- js/main.js
+++ js/main.js
@@ -366,57 +366,13 @@ $(document).ready(function () {
             $(event.target.closest('.myth')).addClass('active');
             document.querySelector('.active').setAttribute('data-order', order);
             let dataAttrib = document.querySelector('.active').getAttribute('data-field');
-            let elements = $('.' + dataAttrib);
-            let tmp = [];
-            let tmp2 = [];
-            $(elements).each(function (index, value) {
-                tmp[index] = replaceOnNumber(String($(value).find('input').val()));
+            let elements = $('.' + dataAttrib).find('input');
+
+            elements.each(function (index, value) {
+                $(value).closest('.mytbody').attr('data-sort', $(value).val());
             })
-            // console.log(tmp)
-            // tmp.forEach(function (key, value, tmp) {
-            // arr1 = temp1.trim().split(" ");
-            // arr2 = temp2.trim().split(" ");
-            // let min
-            // if (arr1.length < arr2.length) {
-            //     min = arr1.length;
-            //     arr2 = arr2.slice(1, arr1.length);
-            // } else {
-            //     min = arr2.length;
-            //     arr1 = arr1.slice(1, arr1.length);
-            // }
-            // for (var i = 0; i < min; i++) {
-            //     if (arr1[i] < arr2[i]) {
-            //
-            //     } else {
-            //
-            //     }
-            // }
-            // });
-            // tmp.forEach(function (key, value, tmp) {
-            //     tmp2[key] = replaceOnNumber(String(value));
-            // });
 
-            // arr1 = temp1.trim().split(" ");
-            // arr2 = temp2.trim().split(" ");
-            // let min
-            // if (arr1.length < arr2.length) {
-            //     min = arr1.length;
-            //     arr2 = arr2.slice(1, arr1.length);
-            // } else {
-            //     min = arr2.length;
-            //     arr1 = arr1.slice(1, arr1.length);
-            // }
-            // for (var i = 0; i < min; i++) {
-            //     if (arr1[i] < arr2[i]) {
-            //
-            //     } else {
-            //
-            //     }
-            // }
-
-
-            sortBodyElements(order, elements);
-            // let result = sortOnPHP(elements, order);
+            sortBodyElements(order);
         }
     });
 
@@ -461,202 +417,22 @@ function remImg() {
 }
 
 //Сортировка элементов
-function sortBodyElements(order = 'asc', elements = '') {
+function sortBodyElements(order = 'asc') {
     var result = $('.mytbody').sort(
         function (a, b) {
-            var contentA = parseInt($(a).data('sort'));
-            var contentB = parseInt($(b).data('sort'));
-            if (order === 'asc') {
-                return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
-            }
-            if (order === 'desc') {
-                return (contentA > contentB) ? -1 : (contentA < contentB) ? 1 : 0;
-            }
+            var contentA = isNumber(parseInt($(a).data('sort'))) ? parseInt($(a).data('sort')) : $(a).data('sort').toLowerCase();
+            var contentB = isNumber(parseInt($(b).data('sort'))) ? parseInt($(b).data('sort')) : $(b).data('sort').toLowerCase();
+
+            return ((order === 'asc') ? 1 : -1) * ((contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0);
         }
     );
     $('.dinamicalBody').html(result);
 }
 
-function sortOnPHP(arr = [], order = 'asc') {
-    var data = new FormData();
-    data.append('sort', true);
-    data.append('order', order);
-    data.append('order', order);
-    $.ajax({
-        type: 'POST',
-        data: data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function (response) {
-            let mytbody = $.parseJSON(response);
-            let template = "<div data-sort=\"" + mytbody.id + "\" class=\"row mytbody\" id=\"mytbody" + mytbody.id + "\">\n" +
-                "    <div class=\"col-sm mytr trid\" hidden>\n" +
-                "        <input type=\"text\" value=\"" + mytbody.id + "\">\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr trfirstname\">\n" +
-                "        <input type=\"text\" value=\"" + mytbody.first_name + "\" readonly>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr trlast_name\">\n" +
-                "        <input type=\"text\" value=\"" + mytbody.last_name + "\" readonly>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr trphone\">\n" +
-                "        <input type=\"text\" value=\"" + mytbody.phone + "\" readonly>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr tremail\">\n" +
-                "        <input type=\"text\" value=\"" + mytbody.email + "\" readonly>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr trphoto\">\n" +
-                "        <input type=\"text\" class=\"bodyImageName\" value=\"" + mytbody.photo + "\" style=\"display: none\">\n" +
-                "        <div class=\"image__wrapper\">\n" +
-                "            <img src=\"/uploads/" + mytbody.photo + "\" class=\"minimized bodyImage\" alt=\"клик для увеличения\"/>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm mytr trbuttons\">\n" +
-                "        <div class=\"tbodyedit showhim\">\n" +
-                "            <div class=\"showme\"><i class=\"far fa-edit\"></i></div>\n" +
-                "            <div class=\"ok\"><i class=\"fas fa-edit\"></i></div>\n" +
-                "        </div>\n" +
-                "        <div class=\"showhim tbodytrash\">\n" +
-                "            <div class=\"showme\"><i class=\"far fa-trash-alt\"></i></div>\n" +
-                "            <div class=\"ok\"><i class=\"fas fa-trash-alt\"></i></div>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "</div>";
-        }
-    });
-}
-
-function genCharArray(charA, charZ) {
-    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
-    for (; i <= j; ++i) {
-        a.push(String.fromCharCode(i));
-    }
-    return a;
-}
-
-function arrayFlip(arr, inc = false) {
-    let exceptions = ['.', '@', '!', '#', '$', '%', '^', "&", '*', '(', ')', '`', '~', '№', ';', ':', '?'];
-    let key;
-    let val;
-    let tmpArr = [];
-    let i = 0;
-    for (key in arr) {
-        val = arr[key];
-
-        if (isNumber(key) > 0) {
-            key = Number(key)
-        } else {
-            key = String(key)
-        }
-        if (isNumber(val) > 0) {
-            val = Number(val)
-        } else {
-            val = String(val)
-        }
-
-        if (exceptions.indexOf(val) === 0) {
-            break;
-        }
-        if (tmpArr.indexOf(val) === -1) {
-            if (inc) {
-                tmpArr[val] = key + 1;
-                tmpArr.length++;
-            } else {
-                tmpArr[val] = key;
-                tmpArr.length++;
-            }
-            i++;
-        } else {
-            break;
-        }
-    }
-    return tmpArr;
-}
 function isNumber(num) {
     let result = [];
     num = String(num);
     result = num.match(/(\d+)/g);
-    return (result !== null) ? result.length : 0;
-}
-function bubbleSort(arr) {
-    let sorted = false;
-    let temp;
-    while (!sorted) {
-        sorted = true;
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                sorted = false;
-            }
-        }
-    }
-    return arr;
+    return (result !== null) ? (result.length > 0) : false;
 }
 
-function replaceOnNumber(str) {
-    str = str.toLowerCase();
-    let templateRus = arrayFlip(genCharArray('а', 'я'), true);
-    let templateEng = arrayFlip(genCharArray('a', 'z'), true);
-    let arr = str.split("");
-    let newStr = '';
-
-    arr = arrayFlip(arr);
-    arr = arraySortValue(arr);
-    arr = bubbleSort(arr);
-
-
-    arr.forEach(function (key, value, arr) {
-        if (getKey(templateRus[key]) === key) {
-            newStr = newStr + ' ' + templateRus[key];
-        }
-        if (getKey(templateEng[key]) === key) {
-            newStr = newStr + ' ' + templateEng[key];
-        }
-    });
-
-    return newStr;
-}
-
-function arraySortValue(arr) {
-    let i = 0;
-    let minNum = -1;
-    let minLet = '';
-    let tmpArr = [];
-    let key; //key => Буквы, arr[key] => цифры
-
-    while (arr.length >= tmpArr.length) {
-        for (key in arr) {
-            if (i === 0) {
-                minNum = arr[key]; //Число
-                minLet = key; //Буква
-            } else {
-                if (arr[key] < minNum) {
-                    minNum = arr[key];
-                    minLet = key;
-                }
-            }
-            i++;
-        }
-
-        tmpArr[minLet] = minNum;
-        tmpArr.length++;
-
-        arr = arrayFlip(arr);
-        let index = arr.indexOf(minNum);
-        if (index !== -1) arr.splice(index, 1);
-        arr = arrayFlip(arr);
-    }
-    return tmpArr;
-}
-
-function getKey(value) {
-    for (var key in this) {
-        if (this[key] == value) {
-            return key;
-        }
-    }
-    return null;
-}
